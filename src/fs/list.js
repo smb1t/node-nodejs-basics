b12efe10cd5e6320869b2708a963e75581e9e80d
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-06-05 21:32:58 +0500

    feat: task file systems - add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,17 @@
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathSrc = `${__dirname}\\files`;
+
 export const list = async () => {
-    // Write your code here 
+  readdir(pathSrc)
+  .then((res) => { // check source folder
+    console.log(res);
+  }).catch (err => {
+    throw new Error('FS operation failed');
+  });
 };
+
+list();
