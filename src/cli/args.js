908c0ee22049bb72231446216c2a2af4a36cf751
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-06-05 22:14:52 +0500

    feat: task Command line interface - add args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
+import { argv, stdout } from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    argv.forEach((val, index) => {
+      if ( val.startsWith('--') && (val.length > 2) && val[2] !== '-' ) {
+        stdout.write(`${val} is ${argv[index + 1]}, `);
+      }
+    });
+
+    stdout.moveCursor(-2, 0);
+    stdout.clearLine(1);
 };
+
+parseArgs();
+
+// node src/cli/args --propName value1 --prop2Name value2
