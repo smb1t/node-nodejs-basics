475c50cfc3d027f9ffe23cdc059d878e4d5c83cf
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-06-05 21:15:08 +0500

    feat: task file systems - add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,18 @@
+import { open, unlink } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file =`${__dirname}\\files\\fileToRemove.txt`;
+
 export const remove = async () => {
-    // Write your code here 
+  open(file, 'r')
+  .then(( )=> {
+    unlink(file);
+    console.log('File deleted.');
+  })
+  .catch((error) => {
+    throw new Error('FS operation failed');
+  });
 };
+
+remove();
