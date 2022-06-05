0ef7bdf17b6c92e72bda1e7e8b1dacf61b7b392e
Alexey Goncharov <alex.g.goncharov@gmail.com>
2022-06-05 22:22:18 +0500

    feat: add solution for task Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import { readFile, writeFile  } from 'fs/promises';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = `${__dirname}\\files\\fileToCalculateHashFor.txt`;
+const hashSum = crypto.createHash('sha256');
+
 export const calculateHash = async () => {
-    // Write your code here 
+  readFile(file).then(() => {
+    const hex = hashSum.update(file).digest('hex');
+    writeFile(file, hex);
+  });  
 };
+
+calculateHash();
